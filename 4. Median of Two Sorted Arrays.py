9568c945cd02ad0b8005b46977547148002a67a4
Kr33gn4r <placek.ireneusz@proton.me>
2023-08-18 14:29:45 +0200

    O(n log n), Runtime - 89 ms, Beats 86.43%

---
+++ 4. Median of Two Sorted Arrays.py
@@ -0,0 +1,6 @@
+from typing import *
+from statistics import median
+
+class Solution:
+    def findMedianSortedArrays(self, nums1: List[int], nums2: List[int]) -> float:
+        return median(sorted(nums1 + nums2))
