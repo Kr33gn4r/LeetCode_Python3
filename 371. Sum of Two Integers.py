6bfa8964d2fadb05b5739d0ed043f81b95bf95d4
Kr33gn4r <placek.ireneusz@proton.me>
2023-08-18 15:25:30 +0200

    O(1), Runtime - 44 ms, Beats 47.51%

---
+++ 371. Sum of Two Integers.py
@@ -0,0 +1,8 @@
+from typing import *
+import math
+class Solution:
+    def getSum(self, a: int, b: int) -> int:
+        if a == 0: return b
+        elif b == 0: return a
+        else: return int(math.log(math.exp(a) * math.exp(b)))
+
