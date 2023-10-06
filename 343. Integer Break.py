e122598d9f47b6c31e89e48d0de5c998434216e6
rskay <placek.ireneusz@gmail.com>
2023-10-06 16:33:29 +0200

    O(n), Runtime - 30 ms, Beats 96.83%

--- 343. Integer Break.py
+++ 343. Integer Break.py
@@ -1,11 +1,9 @@
 class Solution:
     def integerBreak(self, n: int) -> int:
         if n == 2 or n == 3: return n - 1
-        result: int = 1
-        while n - 4 > 0:
-            n -= 3
-            result *= 3
-        return result * n
+        return pow(3, n//3) if n % 3 == 0 else \
+               pow(3, n//3 - 1) * 4 if n % 3 == 1 else \
+               pow(3, n//3) * 2
 
 # uses the fact that for any x, 3^x > x^3 (3 is the closest integer to e which stands for best growth)
 # but 3 * 1 is less than 2 * 2, therefore we stop before we have that.
